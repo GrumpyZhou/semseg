9bdd52053d206e462e7b4fda5045130f6fb14c72
Yu Wang <WY@24.208.eduroam.dynamic.rbg.tum.de>
2016-12-01 10:07:02 +0100

    Add test data image "tabby_cat", test script test_fcn32.py and update data_utils.py

--- core/data_utils.py
+++ core/data_utils.py
@@ -16,9 +16,16 @@ def load_vgg16_weight(path):
     # Initial network params
     path = os.path.abspath(os.path.join(path, os.curdir))
     fpath = os.path.join(path, "vgg16.npy")
-    
+
     data_dict = np.load(fpath, encoding='latin1').item()
     print("Successfullt load vgg16 weight from %s."%fpath)
     print("Data dict keys:\n",data_dict.keys())
     return data_dict
+
+def color_image(image, num_classes=20):
+    import matplotlib as mpl
+    norm = mpl.colors.Normalize(vmin=0., vmax=num_classes)
+    mycm = mpl.cm.get_cmap('Set1')
+    return mycm(norm(image))
+
 
