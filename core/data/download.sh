1d64aafdeb16aa3865cc78d36ba649c36fc234bc
Qunjie Zhou <qunjie.zhou@tum.de>
2016-12-01 17:08:37 +0100

    add link to download PASCAL_VOC2012 dataset

--- core/data/download.sh
+++ core/data/download.sh
@@ -1,1 +1,2 @@
 curl https://dl.dropboxusercontent.com/u/50333326/vgg16.npy --output vgg16.npy
+curl http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar --output pascal_voc_2012.tar
