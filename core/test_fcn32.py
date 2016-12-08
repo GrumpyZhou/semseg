583eb1844d6cd2e2cd9e729223f60f9c495a1c9d
Qunjie Zhou <qunjie.zhou@tum.de>
2016-12-08 16:56:37 +0100

    Cleanning useless code, writing script for class training and testing

--- core/test_fcn32.py
+++ core/test_fcn32.py
@@ -24,19 +24,20 @@ os.environ['CUDA_VISIBLE_DEVICES'] = ''
 
 # Import training and validation dataset
 train_data_config = {'voc_dir':"data/VOC2012",
-                     'dataset':'train',
+                     'dataset':'val',
+                     'classes':['person'],# All classes are loaded if class is None
+                     'filter_no_label':False,# Filter all indices with no ground truth,
+                                             # Set to True only when you know it will happen, 
+                                             #e.g you defined a class
+                                             # Default is false
                      'randomize': True,
                      'seed': None}
+
 params = {'num_classes': 22, 'rate': 1e-4,
-          'trained_weight_path':'data/fcn3_fconv.npy'}
+          'trained_weight_path':'data/train_bird.npy'}
 
-train_dataset = dt.VOCDataSet(train_data_config)
-
-# hyper-parameter
-num_images = 1
-
-# test_img2 = skimage.io.imread("./data/test_img/tabby_cat.png")
-test_img1 = skimage.io.imread("./data/test_img/person_bike.jpg")
+val_dataset = dt.VOCDataSet(train_data_config)
+iterations = 5
 
 with tf.Session() as sess:
     # Init model and load approriate weights-data
@@ -47,27 +48,25 @@ with tf.Session() as sess:
     option={'fcn32s':True, 'fcn16s':False, 'fcn8s':False} 
     predict_ = vgg_fcn32s.inference(image, num_classes=params['num_classes'], random_init_fc8=False, option=option)
 
+    predict = {}    
     print('Finished building inference network-fcn32.')
     init = tf.initialize_all_variables()
     sess.run(init)
-    predict = {}
 
     print('Running the inference ...')
-    for i in range(num_images):
-        print("image ", i)
-        # next_pair = train_dataset.next_batch()
-        # feed_dict = {image: next_pair[0]}
+    for i in range(iterations):
+        print("iter:", i)
+        next_pair = val_dataset.next_batch()
+        idx = val_dataset.indices[val_dataset.idx]
 
-        image_height, image_width = tf.shape(test_img1)[0], tf.shape(test_img1)[1]
-        # convert to numpy integers
-        image_height_val, image_width_val = image_height.eval(), image_width.eval()
-        feed_image = np.reshape(test_img1, (1, image_height_val, image_width_val,3))
-        feed_dict = {image: feed_image}
-        
+        next_pair_image = next_pair[0]
+        feed_dict = {image: next_pair_image}
+
         predict = sess.run(predict_, feed_dict=feed_dict)
-        print( len(predict))
         for key in option.keys():
             if option[key]:
                 pred_color = dt.color_image(predict[key][0], num_classes=params['num_classes'])
-                scp.misc.imsave('./data/test_img/person_bike_%s_3conv_r_nr.png'%key, pred_color)
+                img_fpath = './data/test_img/%s_%s_%s.png'%(train_data_config['class'],key,idx)
+                scp.misc.imsave(img_fpath, pred_color)
+                print('Image saved: %s'%img_fpath)
       
--- core/train_fcn32.py
+++ core/train_fcn32.py
@@ -26,88 +26,91 @@ os.environ['CUDA_VISIBLE_DEVICES'] = ''
 # Import training and validation dataset
 train_data_config = {'voc_dir':"data/VOC2012",
                      'dataset':'train',
+                     'classes':['person','cat','dog'],# All classes are loaded if class is None
+                     'filter_no_label':True, # Filter all indices with no ground truth,
+                                             # Set to True only when you know it will happen, e.g you defined a class
+                                             # Default is false
                      'randomize': True,
                      'seed': None}
+
 params = {'num_classes': 22, 'rate': 1e-4,
           'trained_weight_path':'data/vgg16.npy',
-          'save_trained_weight_path':'data/fcn3_fconv.npy',
-          'predef_index':'2007_000129'} # None if not needed
+          'save_trained_weight_path':'data/train_bird.npy',
+          'predef_index':None}              # None, if not needed
 	
 train_dataset = dt.VOCDataSet(train_data_config)
 
 # Hyper-parameters
-batch_size = 2
 iterations = 10
 
 with tf.Session() as sess:
-	# Init CNN -> load pre-trained weights from VGG16.
-	vgg_fcn32s = FCN16VGG(params['trained_weight_path'])
-	batch = tf.placeholder(tf.float32, shape=[1, None, None, 3])
-	label = tf.placeholder(tf.int32, shape=[None])	# lable is already vectorized before feed
+    # Init CNN -> load pre-trained weights from VGG16.
+    vgg_fcn32s = FCN16VGG(params['trained_weight_path'])
+    batch = tf.placeholder(tf.float32, shape=[1, None, None, 3])
+    label = tf.placeholder(tf.int32, shape=[None])	# label is already vectorized before feed
 
-	sparse_indices = tf.placeholder(tf.int64, shape=[None,2])
-	sparse_values = tf.placeholder(tf.float32, shape=[None])
-	sparse_bias = tf.placeholder(tf.float32, shape=[None])
-	# create model and train op
-        [train_op, loss] = vgg_fcn32s.train_fcn32(params=params,
-                                                  image=batch,
-                                                  truth=label,
-						  diag_indices=sparse_indices,
-                                                  diag_values=sparse_values,
-                                                  add_bias=sparse_bias,
-                                                  random_init_fc8=False,
-                                                  save_var=True)
-        trained_var_dict = vgg_fcn32s.var_dict
-	print('Finished building network-fcn32.')
-	init = tf.initialize_all_variables()
-	sess.run(init)
+    sparse_indices = tf.placeholder(tf.int64, shape=[None,2])
+    sparse_values = tf.placeholder(tf.float32, shape=[None])
+    sparse_bias = tf.placeholder(tf.float32, shape=[None])
+    # create model and train op
+    [train_op, loss] = vgg_fcn32s.train_fcn32(params=params,
+                                              image=batch,
+                                              truth=label,
+                                              diag_indices=sparse_indices,
+                                              diag_values=sparse_values,
+                                              add_bias=sparse_bias,
+                                              random_init_fc8=False,
+                                              save_var=True)
+    trained_var_dict = vgg_fcn32s.var_dict
+    print('Finished building network-fcn32.')
+    init = tf.initialize_all_variables()
+    sess.run(init)
 
-	print('Start training fcn32...')
-	for i in range(iterations):
-		print("iter: ", i)
-		for j in range(batch_size):
-			next_pair = train_dataset.next_batch(params['predef_index'])		                
-                        next_pair_image = next_pair[0]
-                        
-                        image_shape = next_pair_image.shape
-                        num_pixels = image_shape[1] * image_shape[2]
-			next_pair_lable = np.reshape(next_pair[1], num_pixels)	# reshape to numpy 1-D vector in order to extract indices
+    print('Start training fcn32...')
+    for i in range(iterations):
+        print("iter: ", i)
+        next_pair = train_dataset.next_batch(params['predef_index'])		                
+        next_pair_image = next_pair[0]
+        
+        image_shape = next_pair_image.shape
+        num_pixels = image_shape[1] * image_shape[2]
+        next_pair_label = np.reshape(next_pair[1], num_pixels)	# reshape to numpy 1-D vector in order to extract indices
 
-			# create values on the diagonal
-			ii = np.where(next_pair_lable == 255)   # find all indices where element value is 255
-			sparse_values_feed = np.ones(num_pixels, dtype=np.float32)
-			np.put(sparse_values_feed, ii, [0.0])		# the values of sparse_values
+        # create values on the diagonal
+        ii = np.where(next_pair_label == 255)   # find all indices where element value is 255
+        sparse_values_feed = np.ones(num_pixels, dtype=np.float32)
+        np.put(sparse_values_feed, ii, [0.0])		# the values of sparse_values
 
-			single = np.arange(num_pixels, dtype=np.int64)
-			single_indices = np.reshape(single, (num_pixels,1))	# to column vector
-			# to double column vectors, requested by tensorflow sparse tensor creation
-			sparse_indices_feed = np.concatenate((single_indices, single_indices), axis=1)
+        single = np.arange(num_pixels, dtype=np.int64)
+        single_indices = np.reshape(single, (num_pixels,1))	# to column vector
+        # to double column vectors, requested by tensorflow sparse tensor creation
+        sparse_indices_feed = np.concatenate((single_indices, single_indices), axis=1)
 
-			# create the vector to be added to the last column
-			sparse_bias_feed = np.zeros(num_pixels, dtype=np.float32)
-			np.put(sparse_bias_feed, ii, [1.0])
+        # create the vector to be added to the last column
+        sparse_bias_feed = np.zeros(num_pixels, dtype=np.float32)
+        np.put(sparse_bias_feed, ii, [1.0])
 
-			# replace all elements with value 255 with 21 -> params['num_classes']-1.0
-			np.put(next_pair_lable, ii, [params['num_classes']-1.0])
-			feed_dict = {batch: next_pair_image,
-                                     label: next_pair_lable,
-				     sparse_indices: sparse_indices_feed,
-		       	             sparse_values: sparse_values_feed,
-		                     sparse_bias: sparse_bias_feed}
-                        
-			sess.run(train_op, feed_dict)
+        # replace all elements with value 255 with 21 -> params['num_classes']-1.0
+        np.put(next_pair_label, ii, [params['num_classes']-1.0])
+        feed_dict = {batch: next_pair_image,
+                     label: next_pair_label,
+                     sparse_indices: sparse_indices_feed,
+                     sparse_values: sparse_values_feed,
+                     sparse_bias: sparse_bias_feed}
+        
+        sess.run(train_op, feed_dict)
 
-			print('Loss: ', sess.run(loss, feed_dict))
-	print('Finished training fcn32')
+        print('Loss: ', sess.run(loss, feed_dict))
+    print('Finished training fcn32')
 
 
-        # Save weight
-        npy_path = params['save_trained_weight_path']
-        weight_dict = sess.run(trained_var_dict)
-        if len(weight_dict.keys()) != 0:
-            for key in weight_dict.keys():
-                print('Layer: %s  Weight shape: %s   Bias shape: %s'%(key, weight_dict[key][0].shape, weight_dict[key][1].shape))
-                
-            np.save(npy_path, weight_dict)
-            print("trained weights saved: ", npy_path)
+    # Save weight
+    npy_path = params['save_trained_weight_path']
+    weight_dict = sess.run(trained_var_dict)
+    if len(weight_dict.keys()) != 0:
+        for key in weight_dict.keys():
+            print('Layer: %s  Weight shape: %s   Bias shape: %s'%(key, weight_dict[key][0].shape, weight_dict[key][1].shape))
             
+        np.save(npy_path, weight_dict)
+        print("trained weights saved: ", npy_path)
+        
