7fb1fba9732133a0f45c350bb4a6009aaf3cc92f
Grumpy Zhou <jennyzqj00@gmail.com>
2017-01-10 16:01:56 +0100

    Training config: fcn_scale fcn16s, iters 50000, from fcn16s_30000

--- core/run/train_fcn32_city.py
+++ core/run/train_fcn32_city.py
@@ -25,7 +25,7 @@ from network.fcn_vgg16 import FCN16VGG
 import data_utils as dt
 
 # Specify which GPU to use
-os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
 # Change to Cityscape database
 train_data_config = {'city_dir':"../data/CityDatabase",
@@ -34,10 +34,10 @@ train_data_config = {'city_dir':"../data/CityDatabase",
                      'dataset': 'train'}
 
 # Define the scale of the network to be trained
-fcn_scale = 'fcn32s'
+fcn_scale = 'fcn16s'
 params = {'num_classes': 20, 'rate': 1e-6,
           'tsboard_save_path': '../data/tsboard_result/%s'%fcn_scale,
-          'trained_weight_path':'../data/val_weights/fcn32s/city_fcn32s_skip_80000.npy',
+          'trained_weight_path':'../data/val_weights/fcn16s/city_fcn16s_skip_30000.npy',
           'save_trained_weight_path':'../data/val_weights/'}
 
 # Change to Cityscape databse
