7d594607400793d2ad7cf19aecdb3d31447c8812
Yu Wang <wyadolph2@gmail.com>
2017-01-20 16:24:19 +0100

    add method producing individual mask and number of 1s

--- core/network/fcn_instance.py
+++ core/network/fcn_instance.py
@@ -21,20 +21,20 @@ class InstanceFCN8s:
 
     def __init__(self, data_path=None, target_class={11:'person', 13:'car'}):
         # Define classes to be segmented to instance level e.g {11:'person', 13:'car'}
-        self.target_class = target_class 
+        self.target_class = target_class
         self.num_selected = len(target_class)
-        
-        
+
+
         # Load pretrained weight
         data_dict = dt.load_weight(data_path)
         self.data_dict = data_dict
 
         # used to save trained weights
         self.var_dict = {}
-        
+
 
     def _build_model(self, image, num_classes, max_instance, direct_slice, is_train=False, save_var=False, val_dict=None):
-        
+
         model = {}
         if val_dict is None:
             # Not during validation, use pretrained weight
@@ -42,7 +42,7 @@ class InstanceFCN8s:
         else:
             # Duing validation, use the currently trained weight
             feed_dict = val_dict
-            
+
         if save_var:
             # During training, weights are saved to var_dict
             var_dict = self.var_dict
@@ -51,7 +51,7 @@ class InstanceFCN8s:
             var_dict = None
 
 
-        # Step1: build fcn8s and score_out which has shape[H, W, Classes] 
+        # Step1: build fcn8s and score_out which has shape[H, W, Classes]
         model['conv1_1'] = nn.conv_layer(image, feed_dict, "conv1_1", var_dict=var_dict)
         model['conv1_2'] = nn.conv_layer(model['conv1_1'], feed_dict, "conv1_2", var_dict=var_dict)
         model['pool1'] = nn.max_pool_layer(model['conv1_2'], "pool1")
@@ -76,48 +76,48 @@ class InstanceFCN8s:
         model['conv5_3'] = nn.conv_layer(model['conv5_2'], feed_dict, "conv5_3", var_dict=var_dict)
         model['pool5'] = nn.max_pool_layer(model['conv5_3'], "pool5")
 
-        model['conv6_1'] = nn.conv_layer(model['pool5'], feed_dict, "conv6_1", 
-                                         shape=[3, 3, 512, 512], dropout=is_train, 
+        model['conv6_1'] = nn.conv_layer(model['pool5'], feed_dict, "conv6_1",
+                                         shape=[3, 3, 512, 512], dropout=is_train,
                                          keep_prob=0.5, var_dict=var_dict)
 
-        model['conv6_2'] = nn.conv_layer(model['conv6_1'], feed_dict, "conv6_2", 
-                                         shape=[3, 3, 512, 512], dropout=is_train, 
+        model['conv6_2'] = nn.conv_layer(model['conv6_1'], feed_dict, "conv6_2",
+                                         shape=[3, 3, 512, 512], dropout=is_train,
                                          keep_prob=0.5, var_dict=var_dict)
 
-        model['conv6_3'] = nn.conv_layer(model['conv6_2'], feed_dict, "conv6_3", 
-                                         shape=[3, 3, 512, 4096], dropout=is_train, 
+        model['conv6_3'] = nn.conv_layer(model['conv6_2'], feed_dict, "conv6_3",
+                                         shape=[3, 3, 512, 4096], dropout=is_train,
                                          keep_prob=0.5, var_dict=var_dict)
 
-        model['conv7'] = nn.conv_layer(model['conv6_3'], feed_dict, "conv7", 
-                                       shape=[1, 1, 4096, 4096], dropout=is_train, 
+        model['conv7'] = nn.conv_layer(model['conv6_3'], feed_dict, "conv7",
+                                       shape=[1, 1, 4096, 4096], dropout=is_train,
                                        keep_prob=0.5, var_dict=var_dict)
 
-        model['score_fr'] = nn.conv_layer(model['conv7'], feed_dict, "score_fr", 
-                                          shape=[1, 1, 4096, num_classes], relu=False, 
+        model['score_fr'] = nn.conv_layer(model['conv7'], feed_dict, "score_fr",
+                                          shape=[1, 1, 4096, num_classes], relu=False,
                                           dropout=False, var_dict=var_dict)
-        
+
         # Upsample: score_fr*2
         upscore_fr_2s = nn.upscore_layer(model['score_fr'], feed_dict, "upscore_fr_2s",
                                        tf.shape(model['pool4']), num_classes,
                                        ksize=4, stride=2, var_dict=var_dict)
         # Fuse upscore_fr_2s + score_pool4
         in_features = model['pool4'].get_shape()[3].value
-        score_pool4 = nn.conv_layer(model['pool4'], feed_dict, "score_pool4", 
-                                    shape=[1, 1, in_features, num_classes], 
+        score_pool4 = nn.conv_layer(model['pool4'], feed_dict, "score_pool4",
+                                    shape=[1, 1, in_features, num_classes],
                                     relu=False, dropout=False, var_dict=var_dict)
-        
+
         fuse_pool4 = tf.add(upscore_fr_2s, score_pool4)
 
-                  
+
         # Upsample fuse_pool4*2
         upscore_pool4_2s = nn.upscore_layer(fuse_pool4, feed_dict, "upscore_pool4_2s",
                                             tf.shape(model['pool3']), num_classes,
                                             ksize=4, stride=2, var_dict=var_dict)
 
-        # Fuse  upscore_pool4_2s + score_pool3            
+        # Fuse  upscore_pool4_2s + score_pool3
         in_features = model['pool3'].get_shape()[3].value
-        score_pool3 = nn.conv_layer(model['pool3'], self.data_dict, "score_pool3", 
-                                    shape=[1, 1, in_features, num_classes], 
+        score_pool3 = nn.conv_layer(model['pool3'], self.data_dict, "score_pool3",
+                                    shape=[1, 1, in_features, num_classes],
                                     relu=False, dropout=False, var_dict=var_dict)
 
         score_out = tf.add(upscore_pool4_2s, score_pool3)
@@ -136,7 +136,7 @@ class InstanceFCN8s:
             pred_out = tf.argmax(score_out, dimension=3)
             pred_out = tf.cast(pred_out, tf.float32)
             pred_out = tf.reshape(pred_out,[shape[1],shape[2]])
-            
+
             for id in sorted(self.target_class.keys()):
                 where = tf.equal(pred_out, id)
                 indices = tf.where(where)
@@ -146,14 +146,14 @@ class InstanceFCN8s:
                 sub_score.append(mask)
 
         model['semantic_mask'] = tf.concat(3, sub_score)
-        
+
         # Convolve semantic_mask to several stacks of instance masks, each having shape [1, h, w, max_instance]
-        model['instance_mask'] = nn.mask_layer(model['semantic_mask'], feed_dict, "conv_depth_mask", 
-                                               shape=[3, 3, self.num_selected, max_instance], 
+        model['instance_mask'] = nn.mask_layer(model['semantic_mask'], feed_dict, "conv_depth_mask",
+                                               shape=[3, 3, self.num_selected, max_instance],
                                                relu=False, dropout=False, var_dict=var_dict)
 
         # Upsample to original size *8 # Or we have to do it by class
-        model['upmask'] = nn.upscore_layer(model['instance_mask'], feed_dict, 
+        model['upmask'] = nn.upscore_layer(model['instance_mask'], feed_dict,
                                   "upmask", tf.shape(image), self.num_selected * max_instance,
                                   ksize=16, stride=8, var_dict=var_dict)
 
@@ -168,7 +168,7 @@ class InstanceFCN8s:
         gt_masks: stacked instance_masks, shape=[h, w, num_selected], tf.int32
         '''
         # Build model
-        model = self._build_model(image, params['num_classes'], params['max_instance'], direct_slice=direct_slice, is_train=True, save_var=save_var) 
+        model = self._build_model(image, params['num_classes'], params['max_instance'], direct_slice=direct_slice, is_train=True, save_var=save_var)
         pred_masks = model['upmask']
         # Split stack by semantic class
         pred_mask_list = tf.split(3, self.num_selected, pred_masks)
@@ -191,15 +191,15 @@ class InstanceFCN8s:
                 pred = pred_mask_list[i]
                 logits = tf.reshape(pred, [shape[0]*shape[1]*shape[2], params['max_instance']])
                 y = tf.nn.softmax(logits)
-            
+
                 # Hot-one representation of gt
                 gt = gt_mask_list[i]
                 gt = tf.reshape(gt, [-1])
                 y_ = tf.one_hot(gt, params['max_instance'])
-                
+
                 # Calculate cross-entropy
                 cross_entropy = -tf.reduce_sum(y_ * tf.log(y))
-                loss += cross_entropy       
+                loss += cross_entropy
 
         train_step = tf.train.AdamOptimizer(params['rate']).minimize(loss)
         return train_step, loss
@@ -207,18 +207,31 @@ class InstanceFCN8s:
     def inference(self, params, image, direct_slice=True):
         """
         Input: image
-        Return: a stack of masks, shape = [h, w, num_classes], 
+        Return: a stack of masks, shape = [h, w, num_classes],
                 each slice represent instance masks belonging to a class
                 value of each pixel is between [0,max_instance)
         """
         # Build model
-        model = self._build_model(image, params['num_classes'], params['max_instance'], direct_slice=direct_slice, is_train=False) 
+        model = self._build_model(image, params['num_classes'], params['max_instance'], direct_slice=direct_slice, is_train=False)
         pred_masks = model['upmask']
-        
+
         # Split stack by semantic class
         pred_mask_list = tf.split(3, self.num_selected, pred_masks)
         instance_masks = []
-        for i in range(self.num_selected):    
+        for i in range(self.num_selected):
             pred = tf.argmax(pred_mask_list[i], dimension=3)
             instance_masks.append(tf.squeeze(pred))
         return instance_masks
+
+    def get_imask_tuple(gt_sparse, inst_num):
+        '''
+        gt_sparse: H*W*1 sparse gt mask
+        inst_num: 1-30 (MUST not start from 0!)
+        Return: tuple (0/1 mask, #1s)
+        '''
+        where = tf.equal(gt_sparse, inst_num)
+        indices = tf.where(where)
+        val = tf.ones((tf.shape(indices)[0],),tf.float32)
+        mask = tf.sparse_to_dense(indices,[1024, 2048],val)
+
+        return (mask, tf.shape(indices)[0])
