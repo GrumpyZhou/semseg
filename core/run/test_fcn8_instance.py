2be8b6fda75ad855840ccb1b60e62624f5fece5f
Grumpy Zhou <jennyzqj00@gmail.com>
2017-01-23 21:48:28 +0100

    Test script update

--- core/run/test_fcn8_instance.py
+++ core/run/test_fcn8_instance.py
@@ -38,7 +38,7 @@ params = {'num_classes': 20, 'max_instance': 20,
           'trained_weight_path':'../data/val_weights/city_instance_80000.npy'}
 
 test_dataset = dt.CityDataSet(test_data_config)
-iterations = 3
+iterations = 5
 
 
 with tf.Session() as sess:
@@ -61,9 +61,9 @@ with tf.Session() as sess:
         feed_dict = {image: next_pair_image}
         
         predict_ = sess.run(predict, feed_dict=feed_dict)
-        #imsave('../data/test_city_instance/person_%d_80000.png'%i,predict_[0])
-        #imsave('../data/test_city_instance/car_%d_80000.png'%i, predict_[1])
-        pname = '../data/test_city_instance/person_%d_80000.png'%i
-        cname = '../data/test_city_instance/car_%d_80000.png'%i
+        #imsave('../data/test_city_instance/person_%d.png'%i,predict_[0])
+        #imsave('../data/test_city_instance/car_%d.png'%i, predict_[1])
+        pname = '../data/test_city_instance/person_%d.png'%i
+        cname = '../data/test_city_instance/car_%d.png'%i
         toimage(predict_[0], high=params['max_instance'], low=0, cmin=0, cmax=params['max_instance']).save(pname)
         toimage(predict_[1], high=params['max_instance'], low=0, cmin=0, cmax=params['max_instance']).save(cname)
