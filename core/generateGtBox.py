a06bdb88a5609c71d341e9317fc70aa1d32fb514
Yu Wang <wyadolph2@gmail.com>
2017-02-13 02:19:36 +0100

    adjust script to generate gt box with various dimensions

--- core/generateGtBox.py
+++ core/generateGtBox.py
@@ -35,21 +35,24 @@ def cal_precise_positive(img_mask, inst_num):
 
     np_indices = np.array(indices)
     if not np_indices.size:
-        return None, None
+        return None, None, None, None
 
     min_x = indices[0].min()
     min_y = indices[1].min()
+    max_x = indices[0].max()
+    max_y = indices[1].max()
 
-    return min_x, min_y
+    return min_x, min_y, max_x, max_y
 
 def get_precise_posiboxes_of_Mask(fname):
     list_of_preposiboxes = []
     img, img_shape = Stat_Boxes.open_gt_file(fname)
+    print('The shape of the image is: ', img_shape)
 
     for inst_num in range(MAX_instances):
-    	(x_coord, y_coord) = cal_precise_positive(img[:,:,1], inst_num+1)
-        if x_coord is not None:
-            list_of_preposiboxes.append([x_coord, y_coord])
+    	(x_min, y_min, x_max, y_max) = cal_precise_positive(img[:,:,1], inst_num+1)
+        if x_min is not None:
+            list_of_preposiboxes.append([x_min, y_min, x_max, y_max])
 
     return list_of_preposiboxes
 
@@ -58,10 +61,17 @@ def is_positive(precise_pos_list, test_coord):
     '''
     for the test_coord, test if it is a positive box
     '''
+
     for precise_coord in precise_pos_list:
-        if abs(precise_coord[0] - test_coord[0]) <= 16 and abs(precise_coord[1] - test_coord[1]) <= 16:
-            # The test_coord is a positive box
-            return True
+    	# Compute the potential positive box coord
+    	pre_box_coord, pre_box_dim = generate_precise_posiBox(precise_coord)
+    	if pre_box_coord is None:
+    		continue
+    	else:
+    		# Test if the tested coord is considered as a positive box
+    		if abs(pre_box_coord[0] - test_coord[0][0]) <= 16 and abs(pre_box_coord[0] - test_coord[0][1]) <= 16:
+    			# The test_coord is a positive box
+    			return True
 
     return False
 
@@ -71,6 +81,10 @@ def random_pair(rand_type):
     '''
     if rand_type == 'positive':
         rand_num = np.random.randint(-16,16)
+
+    # the random number generated is still between -448 and 448,
+    # since it covers most of the dimensions of boxes.
+    # Attension: may need a better random generator.
     elif rand_type == 'negative':
         rand_num = np.random.randint(-448,448)
         if rand_num >= 0 and rand_num <= 16:
@@ -92,12 +106,19 @@ def generate_box(fname):
 	number_of_preposiboxes = len(list_of_preposiboxes)
 	loop_num = int(128 / number_of_preposiboxes)
 	loop_remainder = int(128 % number_of_preposiboxes)
+	invalid_remainder = 0
 	# Generating 128 positive and negative boxes, respectively.
 	for index in range(loop_num):
 		for pre_coord in list_of_preposiboxes:
 			# Generate positive box
 			posi_box = generate_positive_box(pre_coord)
-			box_data['positive'].append(posi_box)
+
+			# If the posi_box is none, this indicates that the box is invalid, skip this loop
+			if posi_box is None:
+				invalid_remainder += 1
+				continue
+			else:
+				box_data['positive'].append(posi_box)
 
 			# Generate negative box
 			nega_box = generate_negative_box(pre_coord)
@@ -107,59 +128,101 @@ def generate_box(fname):
 			box_data['negative'].append(nega_box)
 
 	# Generate for the remainder boxes.
-	for pre_coord in list_of_preposiboxes:
-		if loop_remainder == 0:
-			break
-		# Generate positive box
-		posi_box = generate_positive_box(pre_coord)
-		box_data['positive'].append(posi_box)
+	loop_remainder += invalid_remainder
+	while loop_remainder != 0:
+		for pre_coord in list_of_preposiboxes:
+			if loop_remainder == 0:
+				break
+			# Generate positive box
+			posi_box = generate_positive_box(pre_coord)
+			if posi_box is None:
+				#loop_remainder += 1
+				continue
+			else:
+				box_data['positive'].append(posi_box)
 
-		# Generate negative box
-		nega_box = generate_negative_box(pre_coord)
-		# If the negative box is not overlap with another positive box
-		while is_positive(list_of_preposiboxes, nega_box):
+			# Generate negative box
 			nega_box = generate_negative_box(pre_coord)
-		box_data['negative'].append(nega_box)
-		loop_remainder -= 1
+			# If the negative box is not overlap with another positive box
+			while is_positive(list_of_preposiboxes, nega_box):
+				nega_box = generate_negative_box(pre_coord)
+			box_data['negative'].append(nega_box)
+			loop_remainder -= 1
 
 		return box_data
 
 def generate_positive_box(precise_posi_coord):
 	'''
 	Compute the randomized upper left corner of positive box.
+	If there's a valid positive box, return it; Otherwise, return None.
 	'''
+
 	rand_shift = random_pair('positive')
-	temp_x = precise_posi_coord[0] + rand_shift
-	temp_y = precise_posi_coord[1] + rand_shift
+	pre_box_coord, pre_box_dim = generate_precise_posiBox(precise_posi_coord)
 
-	# Boundary of y: 1024-448-1
-	if temp_y > 575 or temp_y < 0:
+	if pre_box_coord is None:
+		return None
+
+	# Randomly shift the precise box by +- 16 pixel in both directions
+	temp_x = pre_box_coord[0] + rand_shift
+	temp_y = pre_box_coord[1] + rand_shift
+
+	if temp_y + pre_box_dim[1] >= 1024 or temp_y < 0:
 		# shift to the other direction
-		temp_y = precise_posi_coord[1] - rand_shift
-	# Boundary of x: 2048-392-1
-	if temp_x > 1655 or temp_x < 0:
-		temp_x = precise_posi_coord[0] - rand_shift
+		temp_y = pre_box_coord[1] - rand_shift
+	if temp_x + pre_box_dim[0] >= 2048 or temp_x < 0:
+		temp_x = pre_box_coord[0] - rand_shift
 
-	return (temp_x, temp_y)
+	posi_box = ((temp_x, temp_y), (pre_box_dim[0], pre_box_dim[1]))
+	return posi_box
 
+def generate_precise_posiBox(precise_posi_coord):
+	'''
+	This function generates the precise positive box for the given object.
+	'''
+	x_min = precise_posi_coord[0]
+	y_min = precise_posi_coord[1]
+	x_max = precise_posi_coord[2]
+	y_max = precise_posi_coord[3]
+
+	x_len = x_max - x_min
+	y_len = y_max - y_min
+	pre_box_coord = (x_min - 32, y_min - 32)
+	pre_box_dim = (x_len + 64, y_len + 64)
+
+
+	# Check the box is within the image boundary
+	if pre_box_coord[0] >= 0 and pre_box_coord[1] >= 0:
+		if pre_box_coord[0] + pre_box_dim[0] < 2048:
+			if pre_box_coord[1] + pre_box_dim[1] < 1024:
+				return pre_box_coord, pre_box_dim
+
+	return None, None
 
 def generate_negative_box(precise_posi_coord):
 	'''
 	Compute the randomized upper left corner of negative box.
+	If the precise positive box is valid, then use it to generate negative box,
+	otherwise, return None.
 	'''
 	rand_shift = random_pair('negative')
-	temp_x = precise_posi_coord[0] + rand_shift
-	temp_y = precise_posi_coord[1] + rand_shift
+	pre_box_coord, pre_box_dim = generate_precise_posiBox(precise_posi_coord)
 
-	# Boundary of y: 1024-448-1
-	if temp_y > 575 or temp_y < 0:
+	if pre_box_coord is None:
+		return None
+
+	# Randomly shift the precise box by +- at least 16 pixel in both directions
+	temp_x = pre_box_coord[0] + rand_shift
+	temp_y = pre_box_coord[1] + rand_shift
+
+	if temp_y + pre_box_dim[1] >= 1024 or temp_y < 0:
 		# shift to the other direction
-		temp_y = precise_posi_coord[1] - rand_shift
-	# Boundary of x: 2048-392-1
-	if temp_x > 1655 or temp_x < 0:
-		temp_x = precise_posi_coord[0] - rand_shift
+		temp_y = pre_box_coord[1] - rand_shift
+	if temp_x + pre_box_dim[0] >= 2048 or temp_x < 0:
+		temp_x = pre_box_coord[0] - rand_shift
 
-	return (temp_x, temp_y)
+	nega_box = ((temp_x, temp_y), (pre_box_dim[0], pre_box_dim[1]))
+	return nega_box
 
 def main():
 
