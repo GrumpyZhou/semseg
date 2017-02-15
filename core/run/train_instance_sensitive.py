42985b6e9ae783df44462f77df1b774989eb3874
Grumpy Zhou <jennyzqj00@gmail.com>
2017-02-15 12:03:11 +0100

    update train script

--- core/run/train_instance_sensitive.py
+++ core/run/train_instance_sensitive.py
@@ -34,26 +34,22 @@ train_data_config = {'city_dir':"../data/CityDatabase",
                      'colored_save_path': None,
                      'labelIDs_save_path': None}
 
-# Define the scale of the network to be trained
-fcn_scale = 'fcn8s'
 
 # TODO: adjust params according to instance-sensitive network
-params = {'rate': 1e-6,
-          'tsboard_save_path': '../data/tsboard_result/%s'%fcn_scale,
+params = {'rate': 1e-5,
+          'tsboard_save_path': '../data/tsboard_result/inst_sens',
           'trained_weight_path':'../data/val_weights/fcn8s/city_fcn8s_skip_100000.npy',
-          #'trained_weight_path':'../data/val_weights/city_fcn8s_skip_100000.npy',
           'save_trained_weight_path':'../data/val_weights/'}
 
 # Change to Cityscape databse
 train_dataset = dt.CityDataSet(train_data_config)
 
 # Hyper-parameters
-train_iter = 1
-val_step = 1
+train_iter = 50000
+val_step = 5000
 
 # Logging config
 with tf.Session() as sess:
-    # Init CNN -> load pre-trained weights from VGG16.
     fcn = InstanceSensitiveFCN8s(params['trained_weight_path'])
     npy_path = params['save_trained_weight_path']
 
@@ -65,10 +61,10 @@ with tf.Session() as sess:
     # create model and train op
     [train_op, loss] = fcn.train(image=train_img, gt_mask=train_mask, gt_box=train_box, learning_rate=params['rate'], num_box=256, save_var=True)
     var_dict_to_train = fcn.var_dict
-    #tf.scalar_summary('train_loss', loss)
+    tf.scalar_summary('train_loss', loss)
 
-    #merged_summary = tf.merge_all_summaries()
-    #writer = tf.train.SummaryWriter(params['tsboard_save_path'], sess.graph)
+    merged_summary = tf.merge_all_summaries()
+    writer = tf.train.SummaryWriter(params['tsboard_save_path'], sess.graph)
 
     init = tf.initialize_all_variables()
     sess.run(init)
@@ -87,22 +83,22 @@ with tf.Session() as sess:
                            train_box: next_pair_box,
                            train_mask: next_pair_label}
         sess.run(train_op, train_feed_dict)
-        print('loss %f'% sess.run(loss, train_feed_dict))
+        #print('loss %f'% sess.run(loss, train_feed_dict))
         # Save loss value
-        # if i % 100 == 0:
-        #     summary, loss_value = sess.run([merged_summary, loss], train_feed_dict)
-        #     writer.add_summary(summary, i)
-        #     print('Iter %d Training Loss: %f' % (i,loss_value))
+        if i % 100 == 0:
+             summary, loss_value = sess.run([merged_summary, loss], train_feed_dict)
+             writer.add_summary(summary, i)
+             print('Iter %d Training Loss: %f' % (i,loss_value))
 
         # Save weight for validation
-  #       if i >= val_step and i % val_step == 0:
-  #           train_weight_dict = sess.run(var_dict_to_train)
-  #           print('Saving trained weight after %d iterations... '%i)
-  #           if len(train_weight_dict.keys()) != 0:
-  #               fname = 'city_%s_instance_sensitive_%d.npy'%(fcn_scale,i)
-		# fpath = npy_path+fname
-  #               np.save(fpath, train_weight_dict)
-  #               print("trained weights saved: ", fpath)
+        if i >= val_step and i % val_step == 0:
+             train_weight_dict = sess.run(var_dict_to_train)
+             print('Saving trained weight after %d iterations... '%i)
+             if len(train_weight_dict.keys()) != 0:
+                 fname = 'city_fcn8s_instance_sensitive_%d.npy'%i
+	         fpath = npy_path+fname
+                 np.save(fpath, train_weight_dict)
+                 print("trained weights saved: ", fpath)
     print('Finished training')
 
 
